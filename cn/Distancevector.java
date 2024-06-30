30bb9b57ee6047c5513b03ed3bec71d7de41ebd5
VishnuDath710 <vishnudathrock@gmail.com>
2024-06-30 16:44:44 +0530

    changes

---
+++ cn/Distancevector.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class  Distancevector
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of nodes:");
+        int n=sc.nextInt();
+        System.out.println("Enter the distance matrix:");
+        int matrix[][]=new int[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                matrix[i][j]=sc.nextInt();
+            }           
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Router info for router: "+i+1);
+            System.out.println("Destination\t NextHop\t Dist\t");
+            for(int j=0;j<n;j++)
+            {
+                System.out.println(j+"\t\t"+j+"\t\t"+matrix[i][j]+"\t\t");  
+            }
+        }      
+        sc.close();
+    }
+}
+
+
+// Enter number of nodes:
+// 3
+// Enter the distance matrix:
+// 1 0 99
+// 0 2 99
+// 3 2 0
---
+++ cn/Subnetting.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Subnetting {
+    public static void main(String args[]) {
+        System.out.println("Enter the Network address");
+        Scanner sc = new Scanner(System.in);
+        String ip = sc.next();
+        String[] IPs = ip.split("\\.");
+        int[] IP = new int[IPs.length];
+        for (int i = 0; i < IPs.length; i++) {
+            IP[i] = Integer.parseInt(IPs[i]);
+            // System.out.println(IP[i]);
+        }
+        System.out.println("Enter the number of Subnets");
+        int sub = sc.nextInt();
+        int temp = IP[0];
+        int mask;
+        if (temp < 128) {
+            mask = 8;
+        } else if (temp < 192) {
+            mask = 16;
+        } else if (temp < 224) {
+            mask = 24;
+        } else {
+            System.out.println("Invalid IP for subnetting");
+            return;
+        }
+        int rem = 32 - mask;
+        long incr1 = 0;
+        long incr = ((long) Math.round(Math.pow(2, rem))) / sub;
+        long incr2 = incr;
+        long t1 = 0;
+        // System.out.println(rem+" "+incr1+"\t"+incr2);
+        for (int j = 0; j < sub; j++) {
+            t1 = incr2;
+            // System.out.println(incr2);
+            System.out.print(IP[0]);
+            System.out.print("." + (IP[1] + (incr1 / (256 * 256))) % 256);
+            System.out.print("." + (IP[2] + (incr1 / 256)) % 256);
+            System.out.print("." + (IP[3] + (incr1 % 256)));
+
+            System.out.print("\t-TO-\t");
+            System.out.print(IP[0]);
+            System.out.print("." + (IP[1] + (t1 / (256 * 256)) - 1) % 256);
+
+            System.out.print("." + (IP[2] + (t1 / 256) - 1) % 256);
+            System.out.print("." + (IP[3] + ((t1 - 1) % 256)));
+            System.out.println("\n");
+            incr1 = incr2;
+            incr2 = incr1 + incr;
+        }
+        sc.close();
+    }
+}
